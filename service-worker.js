cfbb8c8adab04f26edd5d58e59a2c8324320b16d
h78vy7cvk7-prog <h78vy7cvk7@privaterelay.appleid.com>
2026-07-23 02:48:24 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -77,3 +77,49 @@ self.addEventListener(
     );
   }
 );
+self.addEventListener(
+  "notificationclick",
+  event => {
+
+    event.notification.close();
+
+    const targetUrl =
+      event.notification.data?.url ||
+      "./";
+
+    event.waitUntil(
+      clients
+        .matchAll({
+          type: "window",
+          includeUncontrolled: true
+        })
+        .then(windowClients => {
+
+          for (
+            const client
+            of windowClients
+          ) {
+            if (
+              "focus" in client
+            ) {
+              client.navigate(
+                targetUrl
+              );
+
+              return client.focus();
+            }
+          }
+
+          if (
+            clients.openWindow
+          ) {
+            return clients.openWindow(
+              targetUrl
+            );
+          }
+
+          return null;
+        })
+    );
+  }
+);
