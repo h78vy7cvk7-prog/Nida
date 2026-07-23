567e81edaeb21eff6b3d9abd6cf76731bafbb22a
h78vy7cvk7-prog <h78vy7cvk7@privaterelay.appleid.com>
2026-07-23 03:12:34 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
-const CACHE_NAME =
-  "nida-v1";
+importScripts(
+  "https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js"
+);
+
+const CACHE_NAME = "nida-v5";
 
 const APP_SHELL = [
   "./",
@@ -11,115 +14,65 @@ const APP_SHELL = [
   "./icons/icon-512.png"
 ];
 
-self.addEventListener(
-  "install",
-  event => {
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+  );
 
-    event.waitUntil(
-      caches
-        .open(CACHE_NAME)
-        .then(cache => {
-          return cache.addAll(
-            APP_SHELL
-          );
-        })
-    );
+  self.skipWaiting();
+});
 
-    self.skipWaiting();
-  }
-);
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 
-self.addEventListener(
-  "activate",
-  event => {
+  self.clients.claim();
+});
 
-    event.waitUntil(
-      caches
-        .keys()
-        .then(keys => {
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(cachedFile => {
+        return cachedFile || fetch(event.request);
+      })
+  );
+});
 
-          const oldCaches =
-            keys.filter(
-              key =>
-                key !== CACHE_NAME
-            );
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
 
-          return Promise.all(
-            oldCaches.map(
-              key =>
-                caches.delete(key)
-            )
-          );
-        })
-    );
+  const targetUrl =
+    event.notification.data?.url || "./";
 
-    self.clients.claim();
-  }
-);
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then(cachedFile => {
-
-          if (cachedFile) {
-            return cachedFile;
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+        includeUncontrolled: true
+      })
+      .then(windowClients => {
+        for (const client of windowClients) {
+          if ("focus" in client) {
+            client.navigate(targetUrl);
+            return client.focus();
           }
+        }
 
-          return fetch(
-            event.request
-          );
-        })
-    );
-  }
-);
-self.addEventListener(
-  "notificationclick",
-  event => {
+        if (clients.openWindow) {
+          return clients.openWindow(targetUrl);
+        }
 
-    event.notification.close();
-
-    const targetUrl =
-      event.notification.data?.url ||
-      "./";
-
-    event.waitUntil(
-      clients
-        .matchAll({
-          type: "window",
-          includeUncontrolled: true
-        })
-        .then(windowClients => {
-
-          for (
-            const client
-            of windowClients
-          ) {
-            if (
-              "focus" in client
-            ) {
-              client.navigate(
-                targetUrl
-              );
-
-              return client.focus();
-            }
-          }
-
-          if (
-            clients.openWindow
-          ) {
-            return clients.openWindow(
-              targetUrl
-            );
-          }
-
-          return null;
-        })
-    );
-  }
-);
+        return null;
+      })
+  );
+});
